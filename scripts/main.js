3a42308ec89f95788e8a316a85c226fc4bb893cb
Calrizer <callumdrain@gmail.com>
2018-05-02 01:19:24 +0100

    Added JS and JQuery

---
+++ scripts/main.js
@@ -0,0 +1,11 @@
+$(document).ready(function(){
+
+    $("#about").click(function(){
+        $(".mainimage").hide();
+    });
+
+    $("#web").click(function(){
+        $(".mainimage").show();
+    });
+
+});
